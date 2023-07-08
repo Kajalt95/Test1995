4873f0a8be09d39ff62e3469fae34fbb38027e44
kajalt95 <kajalr41@gmail.com>
2023-07-07 20:55:17 -0400

    coomit for Heeloagain

---
+++ Helloagain/src/com/model/Helloagain.java
@@ -0,0 +1,22 @@
+package com.model;
+
+public class Helloagain
+{
+
+{
+	System.out.println("Statement of object");
+	
+}
+
+
+
+
+	public static void main (String args[])
+	{
+	System.out.println("Hello java again");
+	Helloagain obj = new Helloagain();
+			
+	}
+
+
+}
